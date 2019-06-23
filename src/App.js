a5ce53c58c3c32687729345ddacf24fa3092e649
cristiantamas <cristiantamas05@gmail.com>
2019-06-23 16:33:04 +0300

    Made ToDoItem component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ToDoItem from './components/ToDoItem';
+import './styles/style.css';
+
+function App() {
+  return (
+    <div className="todo-list">
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/components/ToDoItem.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ToDoItem(){
+    return(
+        <div class="todo-item">
+          <input type="checkbox" />
+          <p>Task to be done</p>
+        </div>
+    );
+}
+
+export default ToDoItem;
